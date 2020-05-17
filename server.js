4f9d15bb5dd2dd0ef72965c30c814acaea72e9ea
roshinir1705 <roshinir1705@gmail.com>
2020-05-17 22:15:29 +0530

    moving lab1 code to github

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app=express();
+
+var server = app.listen(8081,function(){
+    var host=server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s",host,port)
+
+
+})
+app.get('/whoiam/:name',function(req,res){
+    //First read existing users, 
+
+    var name=req.params.name;
+    var output;
+
+    console.log(name);
+    if(name=='Jey'){
+        output=name+" is NEC Alumini";
+    }
+    else{
+        output=name+" is current student";
+    }
+    res.end(JSON.stringify(output));
+});
